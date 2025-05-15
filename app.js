78931416e421d1aaca8395e1a42d2097c39cd1c0
Jiya sharma <sjiya7527@gmail.com>
2025-05-15 22:35:27 +0530

    Initial commit without node_modules

---
+++ app.js
@@ -0,0 +1,126 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const Listing = require("./models/listing.js");
+const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
+const {listingSchema} = require("./schema.js");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname,"views"));
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
+app.engine("ejs",ejsMate);
+app.use(express.static(path.join(__dirname,"/public")));
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+main()
+    .then(() => {
+        console.log("connected to database");
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+
+async function main(){
+    await mongoose.connect(MONGO_URL);
+}
+
+app.get("/", (req,res) => {
+    res.send("hi i am root");
+})
+
+//Validation for schema (server side validation)
+const validateListing = (req, res, next) => {
+    let {error} = listingSchema.validate(req.body);
+        if(error){
+            let errMsg = error.details.map((el) => el.message).join(",");
+            throw new ExpressError(404, errMsg);
+        }else{
+            next();
+        }
+}
+
+//Index route
+app.get("/listings", 
+    wrapAsync(async (req,res) => {
+        const allListings = await Listing.find({});
+        res.render("listings/index.ejs",{allListings});
+    })
+)
+//New Route
+app.get("/listings/new",  (req,res) => {
+    res.render("listings/new.ejs");
+})
+//Show route
+app.get("/listings/:id", 
+    wrapAsync(async (req,res) => {
+        let {id} = req.params;
+        const listing = await Listing.findById(id);
+        res.render("listings/show.ejs",{listing})
+    }) 
+)
+//Create Route
+app.post("/listings", 
+    validateListing,
+    wrapAsync(async (req,res,next) => {
+        const newListing = new Listing(req.body.listing);
+        await newListing.save();
+        res.redirect("/listings");
+    })
+)
+//Edit Route
+app.get("/listings/:id/edit", 
+    wrapAsync(async (req,res) => {
+        let {id} = req.params;
+        const listing = await Listing.findById(id);
+        res.render("listings/edit.ejs", {listing});
+    }) 
+)
+//Update route
+app.put("/listings/:id", 
+    validateListing,
+    wrapAsync(async (req,res) => {
+        let {id} = req.params;
+        await Listing.findByIdAndUpdate(id,{...req.body.listing});//... (spread operator) means it unpacks all the fields inside {}
+        res.redirect(`/listings/${id}`);
+}) 
+)
+//Delete route
+app.delete("/listings/:id", 
+    wrapAsync(async (req,res) => {
+        let {id} = req.params;
+        let deletedListing = await Listing.findByIdAndDelete(id);
+        console.log(deletedListing);
+        res.redirect("/listings");
+})
+)
+
+// app.get(("/testListing"), async (req, res) => {
+//     let sampleListing = await new Listing({
+//         title: "New villa",
+//         description: "By the beach",
+//         price: 12000,
+//         location : "Calangute, Goa",
+//         country : "India",
+//     });
+//     sampleListing.save();
+//     console.log("sample was saved");
+//     res.send("successful testing");
+// });
+app.all(/.*/, (req,res,next) => {
+    next(new ExpressError(404, "Page Not Found!"));
+})
+//Error handling middleware
+app.use((err, req, res, next)=>{
+    let {statusCode=500,message="Something went wrong!"} = err;
+    res.status(statusCode).render("error.ejs",{message});
+//    res.status(statusCode).send(message);
+})
+
+app.listen(8080, () => {
+    console.log("server is listening to port 8080");
+})
